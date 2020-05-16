7b12e74ca7a98795cec502820490f5a723972c9c
Babatunde <babatundea15@gmail.com>
2020-05-16 19:14:53 +0100

    I installed prebaked middleware and created dummy API

--- api/server.js
+++ api/server.js
@@ -1,9 +1,23 @@
+// I imported the pre-baked middleware
 const express = require("express");
+const cors = require("cors");
+const helmet = require("helmet");
+const server = express();
+
+// Link the server and the router so that the endpoints can be accessed
+const accountRouter = require("../accounts/accountRouter");
 
 const db = require("../data/dbConfig.js");
 
-const server = express();
-
+// Here, i am making use of the middleware
 server.use(express.json());
+server.use(cors);
+server.use(helmet);
+server.use("/account", accountRouter);
+
+// Flesh out a dummy API
+server.get("/", (req, res) => {
+  res.send(`<h2>Building accounts endpoint!</h2>`);
+});
 
 module.exports = server;
